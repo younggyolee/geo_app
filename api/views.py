4c45507857fa588166172eb0afc04cf8ed3dc304
Younggyo Lee <52687834+younggyolee@users.noreply.github.com>
2022-12-24 14:31:11 +0900

    Implement basic logic for Point

--- api/views.py
+++ api/views.py
@@ -1,5 +1,19 @@
+import json
 from django.http import HttpResponse
+from django.core import serializers
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.gis.geos import Point
+from django.contrib.gis.db import models
 
+from . import models
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+@csrf_exempt
+def point(request):
+    if request.method == 'GET':
+        data = serializers.serialize('geojson', list(models.Point.objects.all()))
+        return HttpResponse(data, content_type='application/json')
+    elif request.method == 'POST':
+        data = json.loads(request.body)
+        x, y = data['geometry']['coordinates']
+        models.Point.objects.create(point=Point(x, y))
+        return HttpResponse({'ok': True})

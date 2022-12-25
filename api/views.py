5435294b46031ad11ca73185f3806d67963607fa
Younggyo Lee <52687834+younggyolee@users.noreply.github.com>
2022-12-25 23:03:02 +0900

    Refactor

--- api/views.py
+++ api/views.py
@@ -1,44 +1,13 @@
 import json
-from django.contrib.gis.geos import Point, Polygon
+from django.contrib.gis.geos import Point
 from django.http import Http404
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, RetrieveAPIView
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework import serializers
 from rest_framework.exceptions import NotFound
 
 from .models import Point, Contour
-
-def validate_coord(x, y):
-    if not -180 <= x <= 180 or not -90 <= y <= 90:
-        raise serializers.ValidationError('Coordinate out of range. (x, y) should be in this range: (-180 <= x <= 180) AND (-90 <= y <= 90)')
-
-def validate_polygon(polygon: Polygon):
-    if not polygon.simple:
-        raise serializers.ValidationError('Only simple polygons are accepted.')
-
-class PointSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Point
-        fields = ['id', 'data']
-
-    def validate_data(self, value: Point):
-        validate_coord(value.x, value.y)
-        return value
-
-class ContourSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Contour
-        fields = ['id', 'data']
-
-    def validate_data(self, value: Polygon):
-        for x, y in value.coords[0]: validate_coord(x, y)
-        validate_polygon(value)
-        return value
-
-class GEOSGeometrySerializer(serializers.Serializer):
-    type = serializers.CharField()
-    coordinates = serializers.ListField()
+from .serializers import PointSerializer, ContourSerializer, GEOSGeometrySerializer
 
 class PointListView(ListCreateAPIView):
     def get_queryset(self):

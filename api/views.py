2fa7ff809df149993eb818a38a85c92ac949479a
Younggyo Lee <52687834+younggyolee@users.noreply.github.com>
2022-12-25 18:53:30 +0900

    Improve error handling

--- api/views.py
+++ api/views.py
@@ -1,10 +1,12 @@
 import json
 from django.shortcuts import get_object_or_404
 from django.contrib.gis.geos import Point, Polygon
+from django.http import Http404
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, RetrieveAPIView
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import serializers
+from rest_framework.exceptions import NotFound
 
 from .models import Point, Contour
 
@@ -41,12 +43,15 @@ class GEOSGeometrySerializer(serializers.Serializer):
 
 class PointListView(ListCreateAPIView):
     def get_queryset(self):
-        contour = self.request.query_params.get('contour')
-        if contour is None:
+        contour_id = self.request.query_params.get('contour')
+        if contour_id is None:
             return Point.objects.all()
         else:
-            poly = get_object_or_404(Contour, pk=contour)
-            return Point.objects.filter(data__within=poly.data)
+            try:
+                polygon = Contour.objects.get(pk=contour_id)
+            except Contour.DoesNotExist:
+                raise NotFound(f'Contour (id: {contour_id}) does not exist')
+            return Point.objects.filter(data__within=polygon.data)
 
     serializer_class = PointSerializer
 
@@ -67,8 +72,15 @@ class ContourIntersectionView(RetrieveAPIView):
     serializer_class = GEOSGeometrySerializer
 
     def retrieve(self, request: Request, *args, **kwargs):
-        contour1 = self.get_object()
-        contour2 = get_object_or_404(Contour, pk=self.request.query_params.get('contour'))
+        try:
+            contour1 = self.get_object()
+        except Http404:
+            raise NotFound(f'The first contour (id: {kwargs["pk"]}) does not exist')
+        try:
+            contour2_id = self.request.query_params.get('contour')
+            contour2 = Contour.objects.get(pk=contour2_id)
+        except Contour.DoesNotExist:
+            raise NotFound(f'The second contour (id: {contour2_id}) does not exist')
         geom = contour1.data.intersection(contour2.data)
         serializer = self.get_serializer(json.loads(geom.json))
         return Response(data=serializer.data)

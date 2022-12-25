fe397a8aa865b17d772f0801bbd670eecb168454
Younggyo Lee <52687834+younggyolee@users.noreply.github.com>
2022-12-25 18:00:55 +0900

    Refactor

--- api/views.py
+++ api/views.py
@@ -1,22 +1,20 @@
 import json
-from django.contrib.gis.db import models
 from django.shortcuts import get_object_or_404
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, GenericAPIView, RetrieveAPIView
-from rest_framework.mixins import RetrieveModelMixin
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import serializers
 
-from . import models
+from .models import Point, Contour
 
 class PointSerializer(serializers.ModelSerializer):
     class Meta:
-        model = models.Point
+        model = Point
         fields = ['id', 'data']
 
 class ContourSerializer(serializers.ModelSerializer):
     class Meta:
-        model = models.Contour
+        model = Contour
         fields = ['id', 'data']
 
 class GEOSGeometrySerializer(serializers.Serializer):
@@ -27,32 +25,32 @@ class PointListView(ListCreateAPIView):
     def get_queryset(self):
         contour = self.request.query_params.get('contour')
         if contour is None:
-            return models.Point.objects.all()
+            return Point.objects.all()
         else:
-            poly = get_object_or_404(models.Contour, pk=contour)
-            return models.Point.objects.filter(data__within=poly.data)
+            poly = get_object_or_404(Contour, pk=contour)
+            return Point.objects.filter(data__within=poly.data)
 
     serializer_class = PointSerializer
 
 class PointView(RetrieveUpdateDestroyAPIView):
-    queryset = models.Point.objects.all()
+    queryset = Point.objects.all()
     serializer_class = PointSerializer
 
 class ContourListView(ListCreateAPIView):
-    queryset = models.Contour.objects.all()
+    queryset = Contour.objects.all()
     serializer_class = ContourSerializer
 
 class ContourView(RetrieveUpdateDestroyAPIView):
-    queryset = models.Contour.objects.all()
+    queryset = Contour.objects.all()
     serializer_class = ContourSerializer
 
 class ContourIntersectionView(RetrieveAPIView):
-    queryset = models.Contour.objects.all()
+    queryset = Contour.objects.all()
     serializer_class = GEOSGeometrySerializer
 
     def retrieve(self, request: Request, *args, **kwargs):
         contour1 = self.get_object()
-        contour2 = get_object_or_404(models.Contour, pk=self.request.query_params.get('contour'))
+        contour2 = get_object_or_404(Contour, pk=self.request.query_params.get('contour'))
         geom = contour1.data.intersection(contour2.data)
         serializer = self.get_serializer(json.loads(geom.json))
         return Response(data=serializer.data)
